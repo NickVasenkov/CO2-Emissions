f34d9004c7d1e2b4874d9cb38b39fe9aa60edf90
NickVasenkov <136252830+NickVasenkov@users.noreply.github.com>
2023-08-06 11:44:47 -0400

    Search for n_estimators and learning rate

---
+++ studies/1_10.py
@@ -0,0 +1,88 @@
+## This file can be re-run over and over to continue searching.
+
+## CHOOSE MAXIMUM RUNNING TIME:
+HOURS = 0
+MINUTES = 5
+SECONDS = 5
+
+## CHOOSE NUMBER OF TRIALS:
+N_TRIALS = 10000
+
+RUNNING_TIME = HOURS * 3600 + MINUTES * 60 + SECONDS
+
+# CHOOSE THE STUDY
+STUDY_NAME = '1_10'
+
+# Import packages
+import joblib
+import pickle
+import optuna.visualization as vis
+import pandas as pd
+import lightgbm as lgb
+
+# CHOOSE THE DATASET
+train = pd.read_csv('../new_datasets/train_1_10.csv', index_col=0)
+
+# CHOOSE THE NUMBER OF PROCESSORS (will be multiplied by 2)
+N_JOBS = -1
+
+# Load study
+study = joblib.load("{}.pkl".format(STUDY_NAME))
+total_seconds = pd.read_csv('{}_seconds.csv'.format(STUDY_NAME), index_col=0)
+
+# Load the global_variables
+global_variables = pd.read_csv('../global_variables.csv', index_col=0)
+
+SEED = global_variables.loc[0, 'SEED']
+
+
+# The function to minimize
+def train_evaluate(params):
+
+    # Instantiate the classifier
+    model = lgb.LGBMRegressor(random_state=SEED, n_jobs=N_JOBS, **params)
+
+    # Calculate the cross-validation Score
+    from functions.get_score import get_score
+
+    train_score, cross_score, std, sub = get_score(global_variables, train, model=model, update=False, prepare_submission=False)
+
+    return cross_score
+
+
+# The function with the parameters ranges. The ranges can be changed.
+def objective(trial):
+    params = {
+        'learning_rate': trial.suggest_float('learning_rate', 1e-4, 3e-1, log=True),
+        'n_estimators': trial.suggest_int('n_estimators', 5, 120, step=5)
+    }
+    return train_evaluate(params)
+
+
+# Run the session
+study.optimize(objective, timeout=RUNNING_TIME, n_trials=N_TRIALS, n_jobs=N_JOBS)
+total_seconds.iloc[0, 0] = total_seconds.iloc[0, 0] + RUNNING_TIME
+
+# Save back to the file
+joblib.dump(study, "{}.pkl".format(STUDY_NAME))
+with open('{}_params.pkl'.format(STUDY_NAME), 'wb') as f:
+    pickle.dump(study.best_params, f)
+total_seconds.to_csv('{}_seconds.csv'.format(STUDY_NAME))
+
+print("Best trial:", study.best_trial.number)
+print("Best average cross-validation Score:", study.best_trial.value)
+print("Best hyperparameters:", study.best_params)
+total_hours = round(total_seconds.iloc[0, 0] / 3600, 3)
+print("Total running time (hours):", total_hours)
+
+# Plotting optimization history (INTERACTIVE)
+optimization_history_plot = vis.plot_optimization_history(study, error_bar=True)
+optimization_history_plot.show()
+
+# Plotting parameter importance
+param_importance_plot = vis.plot_param_importances(study)
+param_importance_plot.show()
+
+# Plotting a contour plot
+contour_plot = vis.plot_contour(study, params=["n_estimators", "learning_rate"])
+contour_plot.show()
